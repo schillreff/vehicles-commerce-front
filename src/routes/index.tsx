3594c5dbbf1114ad0219528627fe09015429c0f2
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-13 13:54:49 -0400

    feat: routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Login } from '../pages/Login';
+import { RecoverPassword } from '../pages/RecoverPassword';
+import { Register } from '../pages/Register';
+import { Seller } from '../pages/Seller';
+
+export const RoutesMain = () => (
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
+    <Route path='/recover-password' element={<RecoverPassword />} />
+    <Route path='/seller' element={<Seller />} />
+    <Route path='*' element={<Home />} />
+  </Routes>
+);
