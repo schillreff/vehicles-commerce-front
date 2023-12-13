383c1f86dd9feaea9198a90d4fe9057edb1d77e5
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-13 14:08:09 -0400

    feat: modal, routes, footer, navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { Toaster } from 'react-hot-toast';
+import { Footer } from './components/Footer';
+import { CallModal } from './components/Modal/CallModal';
+import { NavBar } from './components/NavBar';
+import { Section } from './components/Section';
+import { GlobalProvider } from './contexts';
+import { RoutesMain } from './routes';
+import { GlobalStyle } from './styles/global';
+import { ResetStyle } from './styles/reset';
+
 function App() {
   return (
-    <>
-      <div></div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <GlobalProvider>
+      <Toaster
+        position='top-center'
+        toastOptions={{
+          style: {
+            background: '#0B0D0D',
+            color: 'white',
+          },
+        }}
+      />
+      <GlobalStyle />
+      <ResetStyle />
+      <NavBar />
+      <CallModal />
+      <Section>
+        <RoutesMain />
+      </Section>
+      <Footer />
+    </GlobalProvider>
   );
 }
 
