2e3e01a467e187fba6b691f7c37bcd901ec68697
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-13 14:00:27 -0400

    feat: container page

---
+++ src/components/ContainerPage/index.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ContainerPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1536px;
+  margin: 100px 0;
+`;
