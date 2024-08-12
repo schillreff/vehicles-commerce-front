08acb4e1bc5970ec5de269c072429a4bb1094c18
Leandro Schillreff <leandroschillreff@gmail.com>
2024-08-12 17:53:30 -0400

    feat: buttons

---
+++ src/components/Button/interfaces.ts
@@ -0,0 +1,10 @@
+export interface IButtonProps {
+  size: string;
+  width?: string;
+  color: string;
+  borderColor: string;
+  backgroundColor: string;
+  hoverColor: string;
+  hoverBorderColor: string;
+  hoverBackgroundColor: string;
+}
