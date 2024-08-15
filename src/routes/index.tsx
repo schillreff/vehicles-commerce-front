ae325f22df6fd84f924a495186e7795275fe7de0
Leandro Schillreff <leandroschillreff@gmail.com>
2024-08-15 14:44:06 -0400

    feat: register page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
 
 export const RoutesMain = () => (
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='*' element={<Home />} />
     <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
   </Routes>
 );
