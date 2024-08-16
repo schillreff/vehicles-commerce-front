4bc4d7ff9954664203092bf74c4bd893ff36df81
Leandro Schillreff <leandroschillreff@gmail.com>
2024-08-16 18:11:54 -0400

    feat: recover password page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Login } from '../pages/Login';
+import { RecoverPassword } from '../pages/RecoverPassword';
 import { Register } from '../pages/Register';
 
 export const RoutesMain = () => (
   <Routes>
     <Route path='/' element={<Home />} />
-    <Route path='*' element={<Home />} />
     <Route path='/login' element={<Login />} />
     <Route path='/register' element={<Register />} />
+    <Route path='/reset-password' element={<RecoverPassword />} />
+    <Route path='*' element={<Home />} />
   </Routes>
 );
