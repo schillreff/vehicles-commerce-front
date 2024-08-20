05c278c70ea2fcb877ceea63ab64fc6bfc21df0a
Leandro Schillreff <leandroschillreff@gmail.com>
2024-08-20 12:56:27 -0400

    feat: create announcement schema

---
+++ src/validators/createAnnouncement.ts
@@ -0,0 +1,25 @@
+import * as yup from 'yup';
+
+export const schema = yup.object().shape({
+  title: yup.string().required('Título obrigatório'),
+  typeSale: yup
+    .string()
+    .required('Tipo da venda obrigatório')
+    .transform((value) => value[0].replace(/["\\]/g, '')),
+  year: yup.string().required('Ano obrigatório'),
+  mileage: yup.string().required('Quilometragem obrigatória'),
+  price: yup.string().required('Preço obrigatório'),
+  description: yup.string().required('Descrição obrigatória'),
+  typeVehicle: yup
+    .string()
+    .transform((value) => value[0].replace(/["\\]/g, ''))
+    .required('Tipo obrigatório'),
+  coverImage: yup
+    .string()
+    .url('Deve ser uma URL válida')
+    .required('Imagem obrigatória'),
+  image1: yup
+    .string()
+    .url('Deve ser uma URL válida')
+    .required('Imagem obrigatória'),
+});
