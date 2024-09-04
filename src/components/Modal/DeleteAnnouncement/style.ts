65a6c97b25973e2ed6d3077cd3c3d246621dfc10
Leandro Schillreff <leandroschillreff@gmail.com>
2024-09-04 13:39:47 -0400

    feat: delete announcement modal

---
+++ src/components/Modal/DeleteAnnouncement/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledDeleteAnnouncement = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  width: 100%;
+
+  p {
+    text-align: justify;
+  }
+
+  .delete-user-form__buttons {
+    display: flex;
+    align-self: flex-end;
+    gap: 16px;
+  }
+`;
